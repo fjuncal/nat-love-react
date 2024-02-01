941b440c5450618976099e1e3334a753f8da94d5
Fellipe Juncal <fjuncal7@gmail.com>
2024-01-31 21:47:27 -0300

    Criando projeto nat-love

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Photo from './components/foto/photo'
+import Natinhaeu from './assets/eu-natinha.jpeg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>NAT-LOVE</h1>
+        <h3>COMING SOON ...</h3>
+        <Photo className="App" endereco={Natinhaeu}/>  
+      </div>
+    </>
   );
 }
 
---
+++ src/components/foto/photo.js
@@ -0,0 +1,7 @@
+function Photo(props){
+    return(
+        <img src={props.endereco} alt="eu-natinha"></img>
+    );
+}
+
+export default Photo;
